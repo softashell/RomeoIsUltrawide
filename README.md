6a5a3e6229e4e0b012ccfdeef31e8bc6b1f404c5
Viliam <wnmerukini@gmail.com>
2026-03-13 22:30:02 +0200

    Initial and hopefully the last commit

--- README.md
+++ README.md
@@ -1,15 +1,44 @@
-# Romeo Is a Dead Man – Ultrawide 21:9 Fix
+# Romeo Is a Dead Man – Ultrawide Semi Fix
 
-Ultrawide (21:9) fix for *Romeo Is a Dead Man*.
+Ultrawide fix for *Romeo Is a Dead Man*.
+
+## How it works?
+- On every engine tick apply constraint adjustments to all UUserWidgets
+
+## How to compile (UI "fixes" only)
+- Dump the sdk using Dumper-7 and paste it into the project directory
+- Open .slnx file using Visual Studio (downgrade vcxproj or use preview version of VS)
+- Build!
+- Rename the DLL to anything you want with .asi extension
+- Place the .asi file next to the game executable (also make sure you have a loader like winmm.dll present in the Binaries folder)
+- Done (if it doesn't compile with static_assert errors make sure you're compiling x64 version, if the error is single just comment the problematic line, lol)
+
+## Installation
+1. Download the latest release
+2. Extract the files
+3. Copy the contents to the game installation folder
+4. Launch the game
+
+## Why?
+The main, the parent repo worked but cutscenes were stretched, UI was very broken and unplayable to me.
+So I had the idea (had experience with UE4/5 and general Reverse Engineering) to create this thing. Just to play.
+Bugs are expected ¯\_(ツ)_/¯
+
+### Screenshots
+| Before | After |
+|:--:|:--:|
+| ![Ingame-Before](https://weesp.in/i/678b64acdcde8865.jpg) | ![Ingame-After](https://weesp.in/i/4af4abf4d6842d3e.jpg) |
+| ![Menu-Before](https://weesp.in/i/1a916e36c3c7cbcb.png) | ![Menu-After](https://weesp.in/i/26b0d72e68cd47c4.png) |
+| ![Cutscenes-Before](https://weesp.in/i/e45987d8e9753b94.png) | ![Cutscenes-After](https://weesp.in/i/088c9306f874706b.jpg) |
+| ![DamageNumbers-Before](https://weesp.in/i/ff78a032058b43dc.jpg) | ![DamageNumbers-After](https://weesp.in/i/847d1a0bcf97a879.jpg) |
 
 ## Credits
 This fix is based on **Lyall's UltrawidePatches** project.
-
-Original repository:
 https://codeberg.org/Lyall/UltrawidePatches
-
 Base patch:
 Crime Boss: Rockay City
+
+and me😝
 
 ## What was changed
 - Folder structure adapted for *Romeo Is a Dead Man*
@@ -24,10 +53,11 @@ Crime Boss: Rockay City
 ## Notes
 - Unofficial community fix
 - May cause issues in cutscenes
-- Tested on 21:9 resolutions
+- Tested on 32:9 resolutions
 - The fix is not fully complete.
 - Some HUD/UI elements may appear misaligned or stretched in ultrawide resolutions.
+- This project is not maintained, I'm only accepting pull requests
+- Thank you Lyall for your work :D 
 
 ## License
-MIT License  
-See LICENSE file for details.
+MIT License
---
+++ RIADMW_SomeUIFixes/dllmain.cpp
@@ -0,0 +1,197 @@
+#include "SDK.hpp"
+#include <thread>
+#include <atomic>
+#include <cstdio>
+#include <MinHook.h>
+
+using namespace SDK;
+
+constexpr float TARGET_ASPECT = 16.0f / 9.0f;
+constexpr int GENGINE_TICK_VTABLE = 94;        // qword_14C033718 + 752LL uhh
+constexpr uintptr_t GENGINE_PTR_RVA = 0xC033718; // qword_14C033718
+
+std::atomic<bool> g_Running{ true };
+std::atomic<bool> g_Ready{ false };
+HMODULE g_Module = nullptr;
+float g_ConstrainedW = 0;
+float g_ConstrainedH = 0;
+UClass* g_WidgetClass = nullptr;
+FILE* g_LogFile = nullptr;
+
+typedef void (*FEngineTick)(UEngine*, float, bool);
+FEngineTick g_OrigTick = nullptr;
+void* g_TickTarget = nullptr;
+
+void Log(const char* fmt, ...)
+{
+	if (!g_LogFile) return;
+	va_list args;
+	va_start(args, fmt);
+	fprintf(g_LogFile, "[RIADM_UI_Semi_Fix] ");
+	vfprintf(g_LogFile, fmt, args);
+	fprintf(g_LogFile, "\n");
+	fflush(g_LogFile);
+	va_end(args);
+}
+
+UWorld* GetWorld() 
+{
+	auto ptr = reinterpret_cast<UWorld**>(InSDKUtils::GetImageBase() + Offsets::GWorld);
+	return ptr ? *ptr : nullptr;
+}
+
+void CalcConstrainedSize(FVector2D vp) 
+{
+	float aspect = vp.X / vp.Y;
+
+	if (aspect > TARGET_ASPECT) 
+	{ 
+		g_ConstrainedH = vp.Y; g_ConstrainedW = vp.Y * TARGET_ASPECT; 
+	}
+	else 
+	{ 
+		g_ConstrainedW = vp.X; g_ConstrainedH = vp.X / TARGET_ASPECT; 
+	}
+}
+
+void HookedEngineTick(UEngine* Engine, float DeltaSeconds, bool bIdleMode) 
+{
+	g_OrigTick(Engine, DeltaSeconds, bIdleMode);
+
+	if (!g_Ready) 
+		return;
+
+	UWorld* World = GetWorld();
+	if (World) 
+	{
+		FVector2D vp = UWidgetLayoutLibrary::GetViewportSize(World);
+		if (vp.X > 0) 
+		{
+			float aspect = vp.X / vp.Y;
+			float newW = (aspect > TARGET_ASPECT) ? vp.Y * TARGET_ASPECT : vp.X;
+			float newH = (aspect > TARGET_ASPECT) ? vp.Y : vp.X / TARGET_ASPECT;
+			if (newW != g_ConstrainedW || newH != g_ConstrainedH) {
+				g_ConstrainedW = newW;
+				g_ConstrainedH = newH;
+				Log("Viewport changed: %.0fx%.0f -> Constrained: %.0fx%.0f",
+					vp.X, vp.Y, g_ConstrainedW, g_ConstrainedH);
+			}
+		}
+	}
+
+	TUObjectArray* Objects = UObject::GObjects.GetTypedPtr();
+	if (!Objects || !g_WidgetClass || g_ConstrainedW <= 0)
+		return;
+
+	for (int32 i = 0; i < Objects->Num(); ++i) 
+	{
+		UObject* Obj = Objects->GetByIndex(i);
+		if (!Obj || Obj->IsDefaultObject() || !Obj->IsA(g_WidgetClass)) 
+			continue;
+
+		UUserWidget* Widget = static_cast<UUserWidget*>(Obj);
+		if (!Widget->IsInViewport()) 
+			continue;
+
+		Widget->SetDesiredSizeInViewport(FVector2D{ g_ConstrainedW, g_ConstrainedH });
+		Widget->SetAlignmentInViewport(FVector2D{ 0.5f, 0.5f });
+		FAnchors anchors;
+		anchors.Minimum = anchors.Maximum = FVector2D{ 0.5f, 0.5f };
+		Widget->SetAnchorsInViewport(anchors);
+	}
+}
+
+bool InstallHook() 
+{
+	uintptr_t base = InSDKUtils::GetImageBase();
+	UEngine* Engine = *reinterpret_cast<UEngine**>(base + GENGINE_PTR_RVA);
+
+	if (!Engine) 
+	{ 
+		Log("ERROR: GEngine is null"); return false;
+	}
+
+	void** vtable = *reinterpret_cast<void***>(Engine);
+	g_TickTarget = vtable[GENGINE_TICK_VTABLE];
+	Log("GEngine: 0x%p  vtable[%d]: 0x%p", Engine, GENGINE_TICK_VTABLE, g_TickTarget);
+
+	if (MH_Initialize() != MH_OK) 
+	{
+		Log("ERROR: MH_Initialize failed"); return false; 
+	}
+
+	if (MH_CreateHook(g_TickTarget, &HookedEngineTick, reinterpret_cast<void**>(&g_OrigTick)) != MH_OK || MH_EnableHook(g_TickTarget) != MH_OK) 
+	{
+		Log("ERROR: Hook creation failed");
+		MH_Uninitialize();
+		return false;
+	}
+
+	Log("Hook installed (trampoline: 0x%p)", g_OrigTick);
+	return true;
+}
+
+void MainThread() 
+{
+	// waiting for accessible to spin up
+	while (g_Running) 
+	{
+		UWorld* World = GetWorld();
+		if (World) // World* should be enabled by this time but anyways
+		{
+			FVector2D vp = UWidgetLayoutLibrary::GetViewportSize(World);
+			if (vp.X > 0)
+			{ 
+				CalcConstrainedSize(vp); 
+				break; 
+			}
+		}
+		Sleep(500);
+	}
+	if (!g_Running) 
+		return;
+
+	Log("Constrained size: %.0fx%.0f", g_ConstrainedW, g_ConstrainedH);
+	g_WidgetClass = UUserWidget::StaticClass();
+
+	if (!InstallHook()) 
+		return; // uh uh, bad thing :(
+
+	g_Ready = true;
+	
+	Log("Running.");
+	
+	while (g_Running)
+	{
+		Sleep(1000);
+	}
+
+	g_Ready = false;
+	
+	MH_DisableHook(g_TickTarget);
+	MH_Uninitialize();
+	
+	Log("Unloaded.");
+
+	fclose(g_LogFile);
+	g_LogFile = nullptr;
+
+	FreeLibraryAndExitThread(g_Module, 0);
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD reason, LPVOID) {
+	if (reason == DLL_PROCESS_ATTACH) {
+		g_Module = hModule;
+		DisableThreadLibraryCalls(hModule);
+
+		char logPath[MAX_PATH];
+		GetModuleFileNameA(hModule, logPath, MAX_PATH);
+
+		char* slash = strrchr(logPath, '\\');
+		if (slash) strcpy_s(slash + 1, MAX_PATH - (slash - logPath) - 1, "srvwidefixes.log");
+		fopen_s(&g_LogFile, logPath, "w");
+
+		std::thread(MainThread).detach();
+	}
+	return TRUE;
+}
